80644da936f47cf0f03fb9297000a16d619c2d10
Arisa <arisa@welovebug.biz>
2026-02-24 10:29:44 +0700

    [Edited]: ปรับการวาง Test step นำ หลาย Action มารวมกันใน Test step เดียว และไปไว้ใน tests/sck-shoppingmall-ui-spec.ts เพื่อให้เข้าใจได้ง่ายขึ้น

--- e2e-ui/tests/sck-shoppingmall-ui.spec.ts
+++ e2e-ui/tests/sck-shoppingmall-ui.spec.ts
@@ -1,20 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Login สำเร็จด้วย username เท่ากับ user_15 password เท่ากับ P@ssw0rd ค้นหาสินค้า Balance Training Bicycle สั่งซื้อ 3 ชิ้ิ้น จัดส่งด้วย  Thaipost และชำระเงินด้วยบัตรเครดิตสำเร็จ', async ({ page }) => {
+test("Login สำเร็จด้วย username เท่ากับ user_15 password เท่ากับ P@ssw0rd ค้นหาสินค้า Balance Training Bicycle สั่งซื้อ 3 ชิ้ิ้น จัดส่งด้วย  Thaipost และชำระเงินด้วยบัตรเครดิตสำเร็จ", async ({ page }) => {
   await test.step("เปิด Browser แล้วไปยังหน้าที่จะทดสอบ", async() => {
     await page.goto("http://139.59.225.96/auth/login");
     });
 
-  await test.step("กรอก User Name", async() => {
+  await test.step("Loin เข้าสู่ระบบ username เท่ากับ user_15 password เท่ากับ P@ssw0rd", async() => {
     await page.locator("#login-username-input").fill("user_15")
-  });
-  
-  await test.step("กรอก Password", async() => {
     await page.locator("#login-password-input").fill("P@ssw0rd")
-  });
-
-  await test.step("กดปุ่ม Login", async () => {
     await page.locator("#login-btn").click();
   });
   
+  
 });
+
